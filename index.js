a2fa2f64a78270f25436fb8ea89b32e120c84700
Mayra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2023-05-14 10:34:49 -0500

    Taller sonarqube

---
+++ index.js
@@ -0,0 +1,8 @@
+function suma(a, b) {
+    return a + b;
+}
+function resta(a, b) {
+    return a - b;
+}
+console.log("La suma de 3 y 4 es:", suma(3, 4));
+console.log("La resta de 7 y 2 es:", resta(7, 2));
